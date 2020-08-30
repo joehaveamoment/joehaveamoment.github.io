d962ad1594b837f8f59480c3a07e1138a4f70872
joehaveamoment <joeinnz@hotmail.com>
2020-08-30 23:18:03 +1200

    index

--- script10.js
+++
@@ -1,7 +0,0 @@
-window.onload = choosePic;
-
-var myPix = new Array("images/a.png","images/i.png","images/u.jpg");
-
-function choosePic() {
-     var randomNum = Math.floor(Math.random() * myPix.length);
-     document.getElementById("myPicture").src = myPix[randomNum];
